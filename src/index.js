16eb84ec4c87c9d8725f21b9710bf1e22f8fa6cc
Adrianna Bell <120890577+thecodedcosmos@users.noreply.github.com>
2023-02-09 22:57:09 -0800

    Attempted to add weather icon changing JS but failed.

--- src/index.js
+++ src/index.js
@@ -67,6 +67,7 @@ function convertToCelsius(event) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = 19;
 }
+
 let dateElement = document.querySelector("#date");
 let date = new Date();
 dateElement.innerHTML = formatDate(date);
@@ -84,3 +85,106 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", convertToCelsius);
 
 searchCity("Atlanta,Ga");
+
+function showIcon() {
+  if (weatherDescription === "Clear" && hours >= 19) {
+    return `images/nightclear.png`;
+  } else {
+    if (weatherDescription === "Clear" && hours <= 06) {
+      return `images/nightclear.png`;
+    } else {
+      if (weatherDescription === "Clear" && hours > 06) {
+        return `images/dayclear.png`;
+      }
+    }
+  }
+}
+if (weatherDescription === "Breezy" && hours >= 19) {
+    return `images/breezynight.png`;
+  } else {
+    if (weatherDescription === "Breezy" && hours <= 06) {
+      return `images/breezynight.png`;
+    } else {
+      if (weatherDescription === "Breezy" && hours > 06) {
+        return `images/breezyday.png`;
+      }
+    }
+  }
+  if (weatherDescription === "Foggy" && hours >= 19) {
+    return `images/foggynight.png`;
+  } else {
+    if (weatherDescription === "Foggy" && hours <= 06) {
+      return `images/foggynight.png`;
+    } else {
+      if (weatherDescription === "Foggy" && hours > 06) {
+        return `images/foggyday.png`;
+      }
+    }
+  }
+  if (weatherDescription === "Misty" && hours >= 19) {
+    return `images/mistynight.png`;
+  } else {
+    if (weatherDescription === "Misty" && hours <= 06) {
+      return `images/mistynight.png`;
+    } else {
+      if (weatherDescription === "Misty" && hours > 06) {
+        return `images/mistyday.png`;
+      }
+    }
+  }
+  if (weatherDescription === "Cloudy" && hours >= 19) {
+    return `images/nightcloudy.png`;
+  } else {
+    if (weatherDescription === "Cloudy" && hours <= 06) {
+      return `images/nightcloudy.png`;
+    } else {
+      if (weatherDescription === "Cloudy" && hours > 06) {
+        return `images/cloudy.png`;
+      }
+    }
+  }
+  if (weatherDescription === "Showery" && hours >= 19) {
+    return `images/showerynight.png`;
+  } else {
+    if (weatherDescription === "Showery" && hours <= 06) {
+      return `images/showerynight.png`;
+    } else {
+      if (weatherDescription === "Showery" && hours > 06) {
+        return `images/showerrain.png`;
+      }
+    }
+  }
+  if (weatherDescription === "Snowy" && hours >= 19) {
+    return `images/snownight.png`;
+  } else {
+    if (weatherDescription === "Snowy" && hours <= 06) {
+      return `images/snownight.png`;
+    } else {
+      if (weatherDescription === "Snowy" && hours > 06) {
+        return `images/daysnow.png`;
+      }
+    }
+  }
+  if (weatherDescription === "Stormy" && hours >= 19) {
+    return `images/stormynight.png`;
+  } else {
+    if (weatherDescription === "Stormy" && hours <= 06) {
+      return `images/stormynight.png`;
+    } else {
+      if (weatherDescription === "Stormy" && hours > 06) {
+        return `images/stormyday.png`;
+      }
+    }
+  }
+  if (weatherDescription === "Sunny" && hours >= 19) {
+    return `images/sunnynight.png`;
+  } else {
+    if (weatherDescription === "Sunny" && hours <= 06) {
+      return `images/sunnynight.png`;
+    } else {
+      if (weatherDescription === "Sunny" && hours > 06) {
+        return `images/sunny.png`;
+      }
+    }
+  }
+  let weatherDescription = null;
