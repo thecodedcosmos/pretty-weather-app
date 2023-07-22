1965d5f09f57b157ffa4ff1856ddf8105c71642a
Adrianna Bell <120890577+thecodedcosmos@users.noreply.github.com>
2023-07-22 07:35:56 -0700

    New API Key

--- src/index.js
+++ src/index.js
@@ -68,7 +68,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "2ff29b57821c3b75b60c68ecd1a8d0dd1aa8d3";
+  let apiKey = "53a4b2b1392fe8f20220772417c83c7f";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayForecast);
 }
